c1092526aee390b07d24de57454e0fa07bb54176
Komala <komala.chandran@capgemini.com>
2018-12-14 23:04:02 -0500

    MetaData complete

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,39 @@
-function buildMetadata(sample) {
-
+function buildMetadata(sample_metadata) {
+  var url = "/metadata/" + sample_metadata;
   // @TODO: Complete the following function that builds the metadata panel
+  var ddiv = d3.select("#sample-metadata");
+  ddiv.html("");
+  
+  
+
+  d3.json(url).then(function(response) {
+    var pdata = document.createElement("p");
+    console.log(response);    
+    // response.key[0]
+    // var row = document.createElement("p");
+    Object.entries(response).forEach(([key, value]) => {
+      
+      // dkey = response.dataset.AGE
+      // dval =  unpack(response.dataset.data, 1);
+
+      // console.log(dkey)
+      // console.log(dval)
+        var cell = ddiv.append("p");
+        var dvalue = key +": " + value
+        cell.text(dvalue);
+        
+        // ddiv.append("p")
+        // .text(function(d){return d.key}) + ": " + text(function(d){return d.value})
+    // });
+  })
+})
+};
+  //
+    
+    
+
+    
+
 
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
@@ -13,11 +46,11 @@ function buildMetadata(sample) {
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-}
+
 
 function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  // @TODO: Build a Pie Chart
   var url = "/samples/" + sample;
   d3.json(url).then(function(response) {
         
@@ -39,12 +72,44 @@ function buildCharts(sample) {
 
     Plotly.newPlot("pie", data, layout);
   });
-}
-    // @TODO: Build a Bubble Chart using the sample data
 
-    // @TODO: Build a Pie Chart
+
+  // @TODO: Build a Bubble Chart using the sample data
+
+  d3.json(url).then(function(response) {
+
+           
+    var data = [{
+      x: response.otu_ids,
+      y: response.sample_values,
+      // text: response.otu_labels,
+      mode:'markers',
+      marker:{
+        size:response.sample_values,
+        color:response.otu_ids
+      }
+      
+    }];
+
+    var layout = {
+      title: "Samples "+sample,
+      xaxis: {
+        title: "OTU ID"
+      },
+      yaxis: {
+        title: "Samples"
+      }
+    };
+
+    Plotly.newPlot("bubble", data, layout);
+  });
+  
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+
+
+}
+    
 
 
 function init() {
